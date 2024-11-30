f5b257217120adc2cab19cfae72cbb0857804478
Adeil Mohammadzadah <145399269+Adeilmo226@users.noreply.github.com>
2024-11-29 15:25:23 -1000

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	  }
 	  let average = sum / bufferLength;
   
-	  return average > 60; // Sensitivity threshold
+	  return average > 40; // Sensitivity threshold
 	}
   
 	function blowOutCandles() {
